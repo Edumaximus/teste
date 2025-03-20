be58c5d72240213845815dcc3da764068b56af32
Eduardo Zaduski <eduardozaduski@gmail.com>
2025-03-20 11:32:12 -0300

    Configuração de acesso ao banco de dados

---
+++ carro/API/Models/AppDataContext.cs
@@ -0,0 +1,11 @@
+using System;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDataContext: DbContext{
+    public DbSet<Carro> Carros {get; set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=eduardo.db");
+    }
+}
---
+++ carro/API/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<Carro> carros = [
+    new Carro (){Id = 1, Name = "Fusca"},
+    new Carro (){Id = 2, Name = "Ferrari"}
+];
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/carros",()=>{
+    return Results.Ok(carros);
+});
+
+app.Run();
